6858c0fd169088a7956e4b064bcf02a033687dd0
usman-digitalks <77572773+usman-digitalks@users.noreply.github.com>
2021-05-27 16:29:40 +0400

    Initial commit

---
+++ app/src/main/java/me/oiz/allplatform/sample/MainActivity.java
@@ -0,0 +1,16 @@
+package me.oiz.allplatform.sample;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import me.oiz.allplatform.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
